d8956a88f357ca6d17d542dc14df57813ed51aaa
platypus-quacks-too <nanyscia@yahoo.it>
2018-11-27 19:13:48 +0100

    Finish the JavaScript gallery.

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
+function activateGallery() {
+  let thumnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumnails.forEach(function (thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      let newImageAlt = thumbnail.dataset.title;
+      mainImage.setAttribute("alt", newImageAlt);
+
+      //  Change which image is current.
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+
+}
